6745fd9eade2d133195c8147759c5401aa8a6ccb
Jiyeong <Jiyeong@192.168.178.32>
2018-01-13 13:12:04 +0100

    Update using class method

--- src/calender/Calender.java
+++ src/calender/Calender.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 
 public class Calender {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public int getMaxDaysOfMonth(int month)
+	
+	{
+		return month;
+	}
+	public void printsampleCalendar(){
 		System.out.println("월 ,화 ,수 ,목 ,금 , 토 ,일");
 		System.out.println("--------------------");
 		System.out.println("1  2  3  4  5  6  7");
@@ -12,7 +16,13 @@ public class Calender {
 		System.out.println("15 16 17 18 19 20 21 ");
 		System.out.println("22 23 24 25 26 27 28 ");
 		
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		Calender cal = new Calender();
+		cal.printsampleCalendar();
 		int month;
 		Scanner input = new Scanner(System.in);
 		System.out.println("달을 입력해주세요");
